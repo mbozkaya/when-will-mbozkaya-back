bb2f6370976407876119edfa11d1c5ffc898842c
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2021-07-25 22:37:10 +0300

    locales

--- src/i18n.js
+++ src/i18n.js
@@ -3,6 +3,20 @@ import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import tr from './locales/tr';
+import en from './locales/en';
+
+const resources = {
+  en: {
+    translation: en
+  },
+  tr: {
+    translation: tr
+  },
+  'en-TR': {
+    translation: tr
+  }
+};
 // don't want to use this?
 // have a look at the Quick start guide 
 // for passing in lng and translations on init
@@ -20,6 +34,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    resources,
     fallbackLng: 'en',
     debug: true,
 
---
+++ src/locales/tr.js
@@ -0,0 +1,8 @@
+export default {
+    "title": "Reacta hoşgeldiniz",
+    "description": {
+        "part1": "To get started, edit <1>src/App.js</1> and save to reload.",
+        "part2": "Switch language between english and german using buttons above."
+    },
+    'test': 'Tehlike yok {{myVar}}'
+}
